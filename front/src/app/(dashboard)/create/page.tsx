b05092df0b36c5a64f5ab4ac7f146bb0284ca098
BoooBass <revive.elephant@gmail.com>
2025-10-08 16:40:07 +0900

    useAuth

---
+++ front/src/app/(dashboard)/create/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import LogoutButton from "@/components/LogoutButton"
+
+const CreateWebPage = () => {
+    return (
+        <div>
+            <h2>ホームページ作成</h2>
+            <LogoutButton />
+        </div>
+    )
+}
+
+export default CreateWebPage
